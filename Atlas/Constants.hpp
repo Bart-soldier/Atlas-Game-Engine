74b4fc1e2c6e27f19f8f42f335492d0f1c194e8f
Bart-soldier <38186911+Bart-soldier@users.noreply.github.com>
2023-04-07 16:56:56 +0200

    Update Constants.hpp

--- Atlas/Constants.hpp
+++ Atlas/Constants.hpp
@@ -11,17 +11,6 @@ const int SCREEN_HEIGHT = 720;
 const int TILESIZE = 16;
 const int TILEFACTOR = 4;
 
-/*enum Directions {
-	NORTH,
-	NORTHEAST,
-	EAST,
-	SOUTHEAST,
-	SOUTH,
-	SOUTHWEST,
-	WEST,
-	NORTHWEST
-};*/
-
 enum Directions {
 	NORTH,
 	EAST,
