6c2aa92b8756307e486db01f2e0769092dca0342
Bart-soldier <38186911+Bart-soldier@users.noreply.github.com>
2023-04-08 16:14:28 +0200

    Made Scene creation clearer

--- Atlas/Scene.cpp
+++ Atlas/Scene.cpp
@@ -10,12 +10,45 @@ Scene::Scene(GraphicsEngine* graphicsEngine) {
 	m_entryY = 0;
 }
 
+Scene::Scene(GraphicsEngine* graphicsEngine, int width, int height) {
+	m_graphicsEngine = graphicsEngine;
+	m_width = width;
+	m_height = height;
+	m_player = nullptr;
+	m_theme = NULL;
+	m_entryX = 0;
+	m_entryY = 0;
+}
+
 Scene::~Scene() {
 	// Free the music
 	if (m_theme != NULL) {
 		Mix_FreeMusic(m_theme);
 		m_theme = NULL;
 	}
+}
+
+void Scene::initializeSceneElements() {
+	for (int y = 0; y < m_height; y++) {
+		for (int x = 0; x < m_width; x++) {
+			m_sceneElements.push_back(std::make_pair(nullptr, nullptr));
+		}
+	}
+
+	// Free unused vector space
+	m_sceneElements.shrink_to_fit();
+}
+
+std::pair<SceneElement*, SceneElement*> Scene::getSceneElement(int x, int y) {
+	return m_sceneElements.at(y * m_width + x);
+}
+
+void Scene::setSceneElementBackground(int x, int y, SceneElement* background) {
+	m_sceneElements.at(y * m_width + x).first = background;
+}
+
+void Scene::setSceneElementForeground(int x, int y, SceneElement* foreground) {
+	m_sceneElements.at(y * m_width + x).second = foreground;
 }
 
 void Scene::addPlayer(Player* player) {
@@ -28,35 +61,34 @@ void Scene::testLevel() {
 	m_width = 10;
 	m_height = 20;
 
+	initializeSceneElements();
+
 	Texture* sand = new Texture(m_graphicsEngine, "resources/images/Sand.png");
 	Texture* tomb = new Texture(m_graphicsEngine, "resources/images/Tomb.png", 3, 4);
 
 	for (int y = 0; y < m_height; y++) {
 		for (int x = 0; x < m_width; x++) {
 			if (y == 0 || y == 1 || y == 2) {
-				if(x == 0) m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, y, 0)));
-				else if(x == m_width - 1) m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, y, 2)));
-				else m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, y, 1)));
+				if (x == 0) setSceneElementForeground(x, y, new Wall(x, y, tomb, y, 0));
+				else if(x == m_width - 1) setSceneElementForeground(x, y, new Wall(x, y, tomb, y, 2));
+				else setSceneElementForeground(x, y, new Wall(x, y, tomb, y, 1));
 			}
 			else if (y == m_height - 1) {
-				if (x == 0) m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, 3, 0)));
-				else if (x == m_width - 1) m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, 3, 2)));
-				else m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, 3, 1)));
+				if (x == 0) setSceneElementForeground(x, y, new Wall(x, y, tomb, 3, 0));
+				else if (x == m_width - 1) setSceneElementForeground(x, y, new Wall(x, y, tomb, 3, 2));
+				else setSceneElementForeground(x, y, new Wall(x, y, tomb, 3, 1));
 			}
-			else if (x == 0) m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, 1, 0)));
-			else if (x == m_width - 1) m_sceneElements.push_back(std::make_pair(nullptr, new Wall(x, y, tomb, 1, 2)));
-			else m_sceneElements.push_back(std::make_pair(new Environment(x, y, sand), nullptr));
+			else if (x == 0) setSceneElementForeground(x, y, new Wall(x, y, tomb, 1, 0));
+			else if (x == m_width - 1) setSceneElementForeground(x, y, new Wall(x, y, tomb, 1, 2));
+			else setSceneElementBackground(x, y, new Environment(x, y, sand));
 		}
 	}
 
 	int x = 4;
 	int y = 4;
 	Wall* ark = new Wall(x, y, new Texture(m_graphicsEngine, "resources/images/ArkCovenant.png"));
-	m_sceneElements.at(y * m_width + x).second = ark;
-	m_sceneElements.at(y * m_width + x + 1).second = ark;
-
-	// Free unused vector space
-	m_sceneElements.shrink_to_fit();
+	setSceneElementForeground(x, y, ark);
+	setSceneElementForeground(x + 1, y, ark);
 
 	m_entryX = (m_width - 1) * TILESIZE * TILEFACTOR / 2;
 	m_entryY = (m_height - 3) * TILESIZE * TILEFACTOR;
--- Atlas/Scene.hpp
+++ Atlas/Scene.hpp
@@ -18,8 +18,14 @@ class Scene {
 
 		Mix_Music* m_theme;
 
+		void initializeSceneElements();
+		std::pair<SceneElement*, SceneElement*> getSceneElement(int x, int y);
+		void setSceneElementBackground(int x, int y, SceneElement* background);
+		void setSceneElementForeground(int x, int y, SceneElement* foreground);
+
 	public:
 		Scene(GraphicsEngine* graphicsEngine);
+		Scene(GraphicsEngine* graphicsEngine, int width, int height);
 		~Scene();
 		void addPlayer(Player* player);
 		void testLevel();
